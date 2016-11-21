4d028fe274020000e7dc2bd5ccc45607a28b4afe
lili <liq1862@gmail.com>
2016-11-21 16:24:21 +0800

    顯示單筆資料

--- app/src/main/java/com/example/user/a20161121_test3/MainActivity.java
+++ app/src/main/java/com/example/user/a20161121_test3/MainActivity.java
@@ -10,6 +10,10 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -27,6 +31,18 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
                         Log.d("NET", response);
+                        try {
+                            JSONArray array = new JSONArray(response);
+                            for (int i=0;i<array.length();i++)
+                            {
+                                JSONObject obj = array.getJSONObject(i);
+                                String str = obj.getString("district");
+                                Log.d("NET", str);
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }, new Response.ErrorListener() {
             @Override
